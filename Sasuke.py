c4fbbfeae8c696bba422efb44104d9f8e7636c42
Dragon emperor <73370803+lyingninja@users.noreply.github.com>
2021-10-31 15:25:59 +0530

    Update Sasuke.py

--- Sasuke.py
+++ Sasuke.py
@@ -1,24 +1,34 @@
 from telegram.ext import Updater
-updater = Updater(token='', use_context=True)
-
-dispatcher = updater.dispatcher
-
+from gtts import gTTS
 import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
-
+updater = Updater(token='2097282130:AAGspMjwMnplLdTqCEhq39JH55NZvyNoNPk', use_context=True)
+dispatcher = updater.dispatcher
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
-
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!I will copy all your text messages")
+    dispatcher.add_handler(echo_handler)
 from telegram.ext import CommandHandler
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
-
 updater.start_polling()
-
 def echo(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+    #context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+    text = update.message.text
+    tts = gTTS(text=text)
+    tts.save('hello.mp3')
+    context.bot.sendAudio(chat_id=update.effective_chat.id,audio = 'hello.mp3')
 
 from telegram.ext import MessageHandler, Filters
 echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
-dispatcher.add_handler(echo_handler)
+#dispatcher.add_handler(echo_handler)
+def stop(update,context):
+   context.bot.send_message(chat_id=update.effective_chat.id, text="I won't say anything from now onwards")
+   dispatcher.remove_handler(echo_handler)
+stop_handler = CommandHandler('stop', stop)
+dispatcher.add_handler(stop_handler)
+def unknown(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
+
+unknown_handler = MessageHandler(Filters.command, unknown)
+dispatcher.add_handler(unknown_handler)
